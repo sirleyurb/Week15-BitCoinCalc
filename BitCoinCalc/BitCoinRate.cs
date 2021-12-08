534d203046932babe4403b93f7b433f704f2e3fd
Opilane TTHK <opilane@tthk.ee>
2021-12-08 10:34:05 +0200

    Week15

--- BitCoinCalc/BitCoinRate.cs
+++ BitCoinCalc/BitCoinRate.cs
@@ -6,5 +6,31 @@ namespace BitCoinCalc
 {
     class BitCoinRate
     {
+        public bpi bpi { get; set; }
+        public string disclaimer { get; set; }
+
+    }
+
+    public class bpi
+    {
+        public USD USD { get; set; }
+    }
+    public class USD
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+
+    public class GBS
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
     }
 }
--- BitCoinCalc/Program.cs
+++ BitCoinCalc/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿
+using System;
+using System.IO;
+using System.Net;
+
+
 
 namespace BitCoinCalc
 {
@@ -6,7 +11,54 @@ namespace BitCoinCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BitCoinRate currentBitCoin = GetRates();
+            Console.WriteLine("Enter the amount of bitcoin");
+            float userCoins = float.Parse(Console.ReadLine());
+            Console.WriteLine("select currency: EUR/USD/GBS");
+            string userCurrency = Console.ReadLine().ToUpper();
+
+            float currentCoinRate = 0;
+            string currencyCode = "";
+
+            if (userCurrency == "EUR")
+            {
+                currentCoinRate = currentBitCoin.bpi.EUR.rate_float;
+            }else if (userCurrency == "USD")
+            {
+                currentCoinRate = currentBitCoin.bpi.USD.rate_float;
+                currencyCode = currentBitCoin.bpi.USD.code; 
+            }else if (userCurrency == "GBS")
+            {
+                currentCoinRate = currentBitCoin.bpi.USD.rate_float;
+                currencyCode = currentBitCoin.bpi.USD.code; 
+            }
+            float result = userCoins * currentCoinRate;
+
+            Console.WriteLine("$Your bitcoins {result} {currencyCode} are worth ");
+
+           //Console.WriteLine($"current rate: {currentBitCoin.bpi.USD.code} {currentBitCoin.bpi.USD.rate_float}");
+            Console.WriteLine($"{currentBitCoin.disclaimer}");
+        }
+
+        public static BitCoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitCoinRate bitCoinData;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitCoinData = JsonConvert.DeserializeObject<BitCoinRate>(response);
+            }
+
+            return bitCoinData;
         }
     }
 }
